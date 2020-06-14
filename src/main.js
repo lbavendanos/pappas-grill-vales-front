3fcd0c34c42cb95411a548277612394a9b690fed
lbavendanos <lbavendanos@gmail.com>
2020-06-14 12:15:57 -0500

    Fix main file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import './styles/app.scss'
+import './plugins'
+import './plugins/vue'
+import './components'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,5 +13,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
