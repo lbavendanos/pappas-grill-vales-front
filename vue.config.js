a29fcad8d9e89d89327a1f644739a2932449b925
lbavendanos <lbavendanos@gmail.com>
2020-06-14 12:13:27 -0500

    Add style sass

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      // @/ is an alias to src/
+      // so this assumes you have a file named `src/variables.sass`
+      // Note: this option is named as "data" in sass-loader v7
+      sass: {
+        prependData: `@import "~@/styles/_variables.scss";`,
+      },
+    },
+  },
 }
