d516d8977783be7c276036b38fca9e02cd5ba56a
lbavendanos <lbavendanos@gmail.com>
2020-06-14 12:14:37 -0500

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,117 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import routes from './routes'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+const router = createRouter()
 
 export default router
+
+/**
+ * The router factory
+ */
+function createRouter() {
+  const vueRouter = new VueRouter({
+    // scrollBehavior,
+    base: process.env.BASE_URL,
+    mode: 'history',
+    routes,
+  })
+
+  vueRouter.beforeEach(beforeEach)
+  vueRouter.afterEach(afterEach)
+
+  return vueRouter
+}
+
+/**
+ * Global router guard.
+ *
+ * @param {Route} to
+ * @param {Route} from
+ * @param {Function} next
+ */
+async function beforeEach(to, from, next) {
+  // Get the matched components and resolve them.
+  const components = await resolveComponents(
+    router.getMatchedComponents({
+      ...to,
+    })
+  )
+
+  if (components.length === 0) {
+    return next()
+  }
+
+  // Start the loading bar.
+  if (components[components.length - 1].loading !== false) {
+    router.app.$nextTick(() => router.app.$Progress.start())
+  }
+
+  next()
+}
+
+/**
+ * Global after hook.
+ *
+ * @param {Route} to
+ * @param {Route} from
+ * @param {Function} next
+ */
+// async function afterEach(to, from, next) {
+async function afterEach() {
+  await router.app.$nextTick()
+  router.app.$Progress.finish()
+}
+
+/**
+ * Resolve async components.
+ *
+ * @param  {Array} components
+ * @return {Array}
+ */
+function resolveComponents(components) {
+  return Promise.all(
+    components.map((component) => {
+      return typeof component === 'function' ? component() : component
+    })
+  )
+}
+
+/**
+ * Scroll Behavior
+ *
+ * @link https://router.vuejs.org/en/advanced/scroll-behavior.html
+ *
+ * @param  {Route} to
+ * @param  {Route} from
+ * @param  {Object|undefined} savedPosition
+ * @return {Object}
+ */
+// function scrollBehavior(to, from, savedPosition) {
+//   if (savedPosition) {
+//     return savedPosition
+//   }
+
+//   if (to.hash) {
+//     return {
+//       selector: to.hash
+//     }
+//   }
+
+//   const [component] = router
+//     .getMatchedComponents({
+//       ...to
+//     })
+//     .slice(-1)
+
+//   if (component && component.scrollToTop === false) {
+//     return {}
+//   }
+
+//   return {
+//     x: 0,
+//     y: 0
+//   }
+// }
---
+++ src/router/routes.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+]
