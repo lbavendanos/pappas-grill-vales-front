d4203cb583675a02fb5cec9b3f609ea7718a1205
lbavendanos <lbavendanos@gmail.com>
2020-06-14 16:22:19 -0500

    Add footer

---
+++ src/components/Footer/FooterMain.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="footer container-fluid bg-brand-tertiary py-2">
+    <div class="row">
+      <div class="col-6">
+        <div class="footer-brand">
+          <img
+            src="./../../assets/logotipo.png"
+            alt="Pappas Grill"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="footer-info">
+          <h6 class="font-weight-light text-white m-0">
+            &copy; 2020 Grupo 03
+          </h6>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  margin-top: auto;
+  padding: 0.5rem 1rem;
+
+  &-brand {
+    width: 5rem;
+  }
+
+  &-info {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
