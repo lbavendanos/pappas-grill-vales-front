284b35314fc3fb23125a0721d6f63f5fcaaa656a
lbavendanos <lbavendanos@gmail.com>
2020-06-14 19:34:41 -0500

    Add const events

---
+++ src/util/events.js
@@ -0,0 +1,10 @@
+export const EVENT_SIDEBAR_TOGGLE_ACTION = 'sidebar.action:toggle'
+export const EVENT_SIDEBAR_CHANGED = 'sidebar.action:changed'
+
+export const EVENT_MODAL_OPEN = 'name.modal:open.index'
+export const EVENT_MODAL_OPENED = 'name.modal:opened.index'
+export const EVENT_MODAL_ERROR = 'name.modal:error.index'
+
+export const CREATE = 'create'
+export const UPDATE = 'update'
+export const DELETE = 'delete'
