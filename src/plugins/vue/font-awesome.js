e50508cb54f88a97e98cfcefe4cfef8f4e5062f2
lbavendanos <lbavendanos@gmail.com>
2020-06-14 19:53:59 -0500

    Add icons

--- src/plugins/vue/font-awesome.js
+++ src/plugins/vue/font-awesome.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import { library, config, dom } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faChartBar, faBars } from '@fortawesome/free-solid-svg-icons'
+import {
+  faHome,
+  faChartBar,
+  faTicketAlt,
+  faTools,
+  faCog,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHome, faChartBar, faBars)
+library.add(faHome, faChartBar, faTicketAlt, faTools, faCog, faBars)
 
 /**
  * Setting this config so that Vue-tables-2 will be able to replace sort icons with chevrons
