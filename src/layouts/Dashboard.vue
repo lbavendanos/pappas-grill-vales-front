594d466810a011a7757bae531f3a2767d43d2bf7
lbavendanos <lbavendanos@gmail.com>
2020-06-14 19:32:46 -0500

    Add design to Dashboard layout

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="">
-    <router-view />
+  <div class="d-flex">
+    <div class="sidebar-wrapper">
+      <sidebar />
+    </div>
+    <div class="dashboard-wrapper">
+      <header class="header">
+        <div class="nav-top">
+          <div class="d-flex justify-content-center align-items-center">
+            <div class="">
+              <sidebar-toggle-button />
+            </div>
+            <ul class="nav d-flex justify-content-end">
+              <li class="nav-item mr-auto">
+                <nav-sequence />
+              </li>
+              <li class="nav-item">
+                <user-dropdown />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <main class="main">
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -8,4 +32,32 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dashboard {
+  background-color: lighten($gray, 19%);
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  &-wrapper {
+    flex-basis: 100%;
+    display: flex;
+    flex-direction: column;
+    // min-height: calc(100vh - 46.94px); // If there is footer
+    min-height: 100vh;
+  }
+}
+
+.sidebar {
+  &-wrapper {
+    min-height: 100%;
+  }
+}
+
+.nav-top {
+  background-color: $sub-menu-color;
+  .nav {
+    width: calc(100% - 60px);
+  }
+}
+</style>
