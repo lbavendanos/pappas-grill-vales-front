74f37cfcb39034374b1a184693d4e3d68bae9ecf
lbavendanos <lbavendanos@gmail.com>
2020-07-07 18:39:05 -0500

    Add functionality to report section

--- src/views/Dashboard/Statistic/Statistic.vue
+++ src/views/Dashboard/Statistic/Statistic.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid container-fluid px-4 my-4">
     <div class="row">
       <div class="col-12">
-        <form class="card mb-3" @submit="onSubmit">
+        <form class="card mb-3" @submit.prevent="onSubmit">
           <div class="card-header">
             Filtros
           </div>
@@ -56,9 +56,16 @@
             </div>
           </div>
           <div class="card-footer">
-            <button class="btn btn-primary mr-1">Aplicar</button>
-            <button class="btn btn-secondary ml-1" @click.prevent="onClear">
-              Limpiar
+            <button
+              type="submit"
+              class="btn btn-primary mr-1"
+              :disabled="isWorking"
+            >
+              <span
+                v-show="isSubmit"
+                class="spinner-border spinner-border-sm"
+              />
+              Aplicar
             </button>
           </div>
         </form>
@@ -102,16 +109,16 @@ export default {
   data() {
     return {
       form: {
-        fechaInicio: null,
-        fechaFin: null,
+        fechaInicio: moment().format('YYYY-MM-DD'),
+        fechaFin: moment().format('YYYY-MM-DD'),
         local: null,
       },
       charts: {
         pie: {
           chartData: [
             ['Tipo', 'Cantidad'],
-            ['Vales canjeados', 1000],
-            ['Vales pendientes', 1170],
+            ['Vales canjeados', 0],
+            ['Vales pendientes', 0],
           ],
           chartOptions: {
             chart: {
@@ -123,10 +130,10 @@ export default {
         bar: {
           chartData: [
             ['Local', 'Cantidad'],
-            ['Miraflores', 1000],
-            ['Cercado', 1170],
-            ['San Isidro', 660],
-            ['Molina', 1030],
+            ['Miraflores', 0],
+            ['Cercado', 0],
+            ['San Isidro', 0],
+            ['Molina', 0],
           ],
           chartOptions: {
             chart: {
@@ -135,11 +142,67 @@ export default {
           },
         },
       },
+      submit: false,
     }
   },
+  computed: {
+    isSubmit() {
+      return this.submit
+    },
+    isWorking() {
+      return this.submit
+    },
+  },
   methods: {
+    getData() {
+      const fechaInicio = moment(this.form.fechaInicio).valueOf()
+      const fechaFin = moment(this.form.fechaFin).valueOf()
+
+      return { fechaInicio, fechaFin }
+    },
+    refreshCharts(value) {
+      const { canjeados } = value
+      const { cantidad } = value
+
+      this.charts.pie.chartData = [
+        ['Tipo', 'Cantidad'],
+        ['Vales canjeados', canjeados],
+        ['Vales pendientes', cantidad - canjeados],
+      ]
+
+      this.charts.bar.chartData = [
+        ['Local', 'Cantidad'],
+        ['Miraflores', 1],
+        ['Cercado', 2],
+        ['San Isidro', 1],
+        ['Molina', 2],
+      ]
+    },
+    startSubmit() {
+      this.submit = true
+    },
+    endSubmit() {
+      this.submit = false
+    },
     onClear() {},
-    onSubmit(e) {},
+    async onSubmit(e) {
+      this.startSubmit()
+
+      const apiUrl = process.env.VUE_APP_API_URL
+      const token = this.auth.user.token
+      const headers = { token }
+      const params = this.getData()
+      const url = `${apiUrl}/vales/dash`
+
+      try {
+        const response = await axios.get(url, { params, headers })
+
+        this.refreshCharts(response.data.resumen)
+      } catch (error) {
+      } finally {
+        this.endSubmit()
+      }
+    },
   },
 }
 </script>
